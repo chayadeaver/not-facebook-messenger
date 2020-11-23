e39b4c6638dd0a6c114290febb373956e81ba5cf
chayadeaver <chayadeaver@gmail.com>
2020-11-23 13:57:51 -0500

    clearing out initial react code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+
+      <input value={input} onChange={e => setInput(e.target.value)}/>
+      <button>Send Message</button>
+      {/* input */}
+      {/* button */}
+
+      {/* messages themselves */}
     </div>
   );
 }
