6b44a8e75aa8349d9527d3272bcc04ded0a70203
chayadeaver <chayadeaver@gmail.com>
2020-11-29 16:12:06 -0500

    imported db and mapped through collection

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Message from './Message';
+import db from './firebase';
 
 function App() {
   const [input, setInput] = useState("")
-  const [messages, setMessages] = useState([
-    {username: "sonny", text: "hey guys"}, 
-    {username: "qazi", text: "whats up"}
-  ])
+  const [messages, setMessages] = useState([])
   const [username, setUsername] = useState("")
+
+  useEffect(() => {
+    db.collection('messages').onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [])
 
   useEffect(() => {
     setUsername(prompt("Please enter your name"))
