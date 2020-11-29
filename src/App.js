146a654c809b03f6b75276caadd34ba1ad059f7e
chayadeaver <chayadeaver@gmail.com>
2020-11-28 21:46:45 -0500

    input fields

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
 
 function App() {
   const [input, setInput] = useState("")
+  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState("")
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"))
+  }, []) 
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    setMessages([...messages, input])
+    setInput("")
+  }
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h2>Welcome {username} </h2>
 
-      <input value={input} onChange={e => setInput(e.target.value)}/>
-      <button>Send Message</button>
+      <form>
+
+      <FormControl>
+        <InputLabel>Enter a message...</InputLabel>
+        <Input value={input} onChange={e => setInput(e.target.value)}/>
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      </FormControl>
+        
+      </form>
+      
       {/* input */}
       {/* button */}
 
       {/* messages themselves */}
+      {
+        messages.map((message, i) => (
+          <Message text={message} />
+
+        ))
+      }
     </div>
   );
 }
