af88c2a251679a250fff681340b8aaf8a9f2a727
chayadeaver <chayadeaver@gmail.com>
2020-12-02 15:42:11 -0500

    added logic for no user

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,12 @@
 import { Card, CardContent, Typography } from '@material-ui/core'
-import React from 'react'
+import React, { forwardRef } from 'react'
 import "./Message.css"
 
-function Message({ username, message }) {
+const Message = forwardRef(({ username, message }, ref) => {
     const isUser = username === message.username
 
     return (
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card className={isUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent>
                     <Typography
@@ -14,13 +14,13 @@ function Message({ username, message }) {
                         variant="h5"
                         component="h2"
                     >
-                        {message.username}: {message.message}
+                        {!isUser && `${message.username || "Unknown "}:` } {message.message}
                     </Typography>
                 </CardContent>
             </Card>
         </div>
         
     )
-}
+})
 
 export default Message
