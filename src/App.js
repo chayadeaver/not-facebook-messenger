53783119a3e71ca1084f63d11832a264ae1eb9e8
chayadeaver <chayadeaver@gmail.com>
2020-12-02 15:42:53 -0500

    message functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import { FormControl, Input } from '@material-ui/core';
 import Message from './Message';
 import db from './firebase';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move'
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
+
 
 function App() {
   const [input, setInput] = useState("")
@@ -10,8 +15,10 @@ function App() {
   const [username, setUsername] = useState("")
 
   useEffect(() => {
-    db.collection('messages').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+    db.collection('messages')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
     })
   }, [])
 
@@ -21,30 +28,36 @@ function App() {
 
   const sendMessage = (e) => {
     e.preventDefault()
-    setMessages([
-      ...messages, { username: username, text: input }
-    ])
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
     setInput("")
   }
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <img src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=100&h=100" alt="facebook-messenger-icon"/>
+      <h1>!Facebook Messenger</h1>
       <h2>Welcome {username} </h2>
 
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
-          <Input value={input} onChange={e => setInput(e.target.value)}/>
-          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder="Enter a message..." value={input} onChange={e => setInput(e.target.value)}/>
+          <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
         </FormControl>
       </form>
 
-      {
-        messages.map((message) => (
-          <Message username={username} message={message} />
-        ))
-      }
+      <FlipMove>
+        {
+          messages.map(({id, message}) => (
+            <Message key={id} username={username} message={message} />
+          ))
+        }
+      </FlipMove>
     </div>
   );
 }
